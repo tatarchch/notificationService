0c75061956e1c67a8a84d5bcfb667defba34154c
tatarchch <tatarnirov15277@mail.ru>
2025-05-21 19:31:23 +0300

    kafka and other

--- src/main/java/com/example/notificationService/dto/RecordDto.java
+++ src/main/java/com/example/notificationService/dto/RecordDto.java
@@ -1,6 +1,5 @@
 package com.example.notificationService.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -12,8 +11,6 @@ public class RecordDto {
 
     private String timing;
 
-    //@JsonFormat(shape = JsonFormat.Shape.ARRAY)
-    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
 
     private String status;
